34d5a780a0f10eb9581894c98069cca55ed9fa84
gilson-delgado <gcdelgaa@gmail.com>
2024-07-08 15:58:41 -0300

    nada modificado

---
+++ src/components/cards/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './cards.css'
+class Card extends React.Component {
+    render() { 
+        return (
+            <div className="card">
+                <h2>Titulo</h2>
+                <p>esse é um texto do card</p>
+    
+            </div>
+        )
+    }
+    }
+
+export default Card
